f965d8e11048db69a17b4125e96cf1df1a81964f
Alexander Riccio <alexander@riccio.com>
2015-10-30 23:42:52 -0400

    Fix tiny things.
    I'm a competent python programmer now.

--- stroop_test.py
+++ stroop_test.py
@@ -1,10 +1,10 @@
-from __future__ import print_function
+﻿from __future__ import print_function
 import colorama
 import random
 import sys
 
 COLOR_DICTIONARY = {'BLACK' : colorama.Fore.BLACK , 'RED': colorama.Fore.RED, 'GREEN': colorama.Fore.GREEN, 'YELLOW':colorama.Fore.YELLOW, 'BLUE': colorama.Fore.BLUE, 'MAGENTA' : colorama.Fore.MAGENTA, 'CYAN' : colorama.Fore.CYAN , 'WHITE': colorama.Fore.WHITE}
-COLOR_LIST = ['BLACK' , 'RED', 'GREEN', 'YELLOW', 'BLUE', 'MAGENTA', 'CYAN', 'WHITE']
+#COLOR_LIST = ['BLACK' , 'RED', 'GREEN', 'YELLOW', 'BLUE', 'MAGENTA', 'CYAN', 'WHITE']
 
 if 'win' in sys.platform:
     #print('win')
@@ -15,9 +15,9 @@ else:
     DEFAULT_FOREGROUND = 'BLACK'
     
 def randSeq(seq, color):
-    randColor = random.choice(seq)
+    randColor = random.choice(list(seq))
     if randColor == color:
-        return randSeq(seq, color)
+        return randSeq(list(seq), list(color))
     else:
         return randColor
 
@@ -28,14 +28,16 @@ def main():
     print(colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.RESET_ALL)
     print("Assuming " + DEFAULT_BACKGROUND + " is the background color, and " + DEFAULT_FOREGROUND + " is the foreground color\n\n")
     correctAnswers = []
-    for color in COLOR_LIST:
-        randColor = randSeq(COLOR_LIST, random.choice(COLOR_LIST))
+    for color in COLOR_DICTIONARY.keys():
+        randColor = randSeq(COLOR_DICTIONARY.keys(), random.choice(list(COLOR_DICTIONARY.keys())))
         if (color != DEFAULT_BACKGROUND) and (color != randColor):
             print(COLOR_DICTIONARY[color] + randColor)
             correctAnswers.append(color)
     print()
+    print(colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.RESET_ALL)
     print('Correct Answers:')
     print(correctAnswers)
+
 
 if __name__ == '__main__':
     main()
